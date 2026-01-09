8d571e11b85519cd9c0c34208dfb780a36d41af3
dishaaaaaa <183928797+mostafa947@users.noreply.github.com>
2026-01-09 19:45:55 +0200

    feat: implement functions to load and save student data

---
+++ enrollment.py
@@ -0,0 +1,21 @@
+import argparse  # مكتبه جهزة عشان فكرة ال CLi argument
+import json      # مكتبة ال json 
+import os        #  عشان نسال النظام عن الملفات
+
+file_name = "students.json"
+
+def loud_data ():
+    if not os.path.exists(file_name):
+        return [] # بيعمل check لو الفايل مش موجود بيبدا ب ليست 
+    try:
+        with open(file_name, 'r') as f:
+            return json.load(f)
+    except json.JSONDecodeError:
+        return []# بيشوف لو الملف موجود بيجيب الداتا الي فيه لو فاضي او فيه eror بيرجع ليست فاضية 
+def save_data (data):
+    with open(file_name, 'w') as f:
+        json.dump(data, f, indent=4)#بيحول الداتا ل json و يكتبه جوا الملف و يحفظه
+        
+def add_student ():
+    data = loud_data
+    

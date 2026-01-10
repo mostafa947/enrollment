957e0574557d974c971374a1b355925d4d189898
dishaaaaaa <183928797+mostafa947@users.noreply.github.com>
2026-01-10 14:52:01 +0200

    refactor: improve comments for load_data and save_data functions

--- enrollment.py
+++ enrollment.py
@@ -9,12 +9,12 @@ def load_data ():
         return [] # بيعمل check لو الفايل مش موجود بيبدا ب ليست 
     try:
         with open(file_name, 'r') as f:
-            return json.load(f)
+            return json.load(f)#Deserialization
     except json.JSONDecodeError:
         return []# بيشوف لو الملف موجود بيجيب الداتا الي فيه لو فاضي او فيه eror بيرجع ليست فاضية 
 def save_data (data):
-    with open(file_name, 'w') as f:
-        json.dump(data, f, indent=4)#بيحول الداتا ل json و يكتبه جوا الملف و يحفظه
+    with open(file_name, 'w') as f:#بيحول الداتا ل json و يكتبه جوا الملف و يحفظه
+        json.dump(data, f, indent=4)#Serialization
         
 def add_student ():
     data = load_data

aba9ee1300f6081061f1332c477f117c50076c9f
Amira Barghout <amiramohammadbarghout@gmail.com>
2026-01-11 03:08:01 +0200

    fix: allow hyphens and whitespace in student name, make course name case-insensitive

--- parse.py
+++ parse.py
@@ -1,34 +1,54 @@
 import argparse
 
-courses = ()
+
+courses = ('cs109', 'computer vision')
 def parser()->argparse.Namespace:
     parser = argparse.ArgumentParser()
     sub_parsers = parser.add_subparsers()
 
     add_parser = sub_parsers.add_parser('add')
-    add_parser.add_argument('name', nargs= '+', type= name, help= 'the name of the student')
-    add_parser.add_argument('id', nargs= 1, type= id, help= 'the id of the student')
+    add_parser.add_argument(
+        'name', 
+        type= name, 
+        help= 'the name of the student')
+    add_parser.add_argument(
+        'id', 
+        type= sid, 
+        help= 'the id of the student')
     #add_parser.set_defaults(func= add_student)
 
     enroll_parser = sub_parsers.add_parser('enroll')
-    enroll_parser.add_argument('id', type= id, help= 'the id of the student')
-    enroll_parser.add_argument('courses', nargs= '+', choices= courses)
+    enroll_parser.add_argument(
+        'id', 
+        type= sid, 
+        help= 'the id of the student')
+    enroll_parser.add_argument(
+        'courses', 
+        type= course, 
+        nargs= '+', 
+        help= f'the courses to enroll the student in from {courses}')   
     #enroll_parser.set_defaults(func= enroll_student)
     
-    display_parser = sub_parsers.add_parser('display')
+    display_parser = sub_parsers.add_parser('display', help= 'display all student')
     #display_parser.set_defaults(func= display)
 
     return parser.parse_args() 
 
 
 def name(n: str)->str:
-    if not n.isalpha():
-        raise argparse.ArgumentTypeError('name can\'t have non-alphabetical characters')
+    n = n.strip()
+    if not n:
+        raise argparse.ArgumentTypeError('the name of the student must be provided')
     
-    return n.capitalize()
+    non_alpha_chars = filter(lambda c: not c.isalpha(), (c for c in n))
+    for c in non_alpha_chars:
+        if c != '-' and c != ' ':
+            raise argparse.ArgumentTypeError(f'\'{c}\' is an invalid character in name')
+
+    return ' '.join([_n.capitalize() for _n in n.split()])
 
 
-def id(_id: str)->int:
+def sid(_id: str)->int:
     try:
         v = int(_id)
     except ValueError:
@@ -38,3 +58,11 @@ def id(_id: str)->int:
         raise argparse.ArgumentTypeError('id must be a number greater than 0')
     
     return v
+
+
+def course(c: str)->str:
+    if c.lower() not in courses:
+        raise argparse.ArgumentTypeError(f'unknown course {c}: expected {courses}')
+    
+    return c.lower()
+
